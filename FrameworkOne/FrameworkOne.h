9aba9cf66ff5c3a9ecaab80ea504e86ccf8b472b
Stephen Anthony <steve@darjeelingapps.com>
2025-07-28 15:41:19 +0100

    Initial Commit

---
+++ FrameworkOne/FrameworkOne.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkOne.h
+//  FrameworkOne
+//
+//  Created by Stephen Anthony on 28/07/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkOne.
+FOUNDATION_EXPORT double FrameworkOneVersionNumber;
+
+//! Project version string for FrameworkOne.
+FOUNDATION_EXPORT const unsigned char FrameworkOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkOne/PublicHeader.h>
+
+
